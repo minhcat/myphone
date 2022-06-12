4a3d56b2d3b2460154b178d2c041c191a4565558
minh cat <taminhcatcn12@gmail.com>
2022-06-12 15:30:51 +0700

    feat: make product validation request #11

---
+++ Modules/Product/Http/Requests/ProductRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\Product\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'      => 'required|unique:products|max:100',
+            'brand_id'  => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required'     => 'A name is required',
+            'name.unique'       => 'A name is unique',
+            'name.max'          => 'A name is max 100',
+            'brand_id.required' => 'A brand_id is required',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator) 
+    {
+
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'error' => $errors,
+                'status_code' => 422,
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
