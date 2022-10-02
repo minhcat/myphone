8785d52076e7e0dce09aee5c0bcabdeaf7b11c51
Nhu Pham <nhu@common-servies.com>
2022-10-02 22:15:05 +0700

    feat: make migration product logs #11

---
+++ Modules/Product/Database/Migrations/2022_10_02_220532_create_product_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('product_id');
+            $table->string('log');
+            $table->tinyInteger('type');
+            $table->unsignedInteger('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_logs');
+    }
+}
